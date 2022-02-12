bce1e7a76ce1e7e8965d57a45029c92721268934
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-02-12 19:31:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//Elements
+const qrdata_elm = document.querySelector("#qr-data")
+const qrcode_elm = document.querySelector("#qr-code")
+
+// QRCode
+const qrcode = new QRCode(qrcode_elm)
+// QR-Code generator
+const generateQrCode = (value) => {
+  qrcode.makeCode(value)
+}
+
+// Events
+qrdata_elm.addEventListener("change", () => {
+  generateQrCode(qrdata_elm.value)
+})
+qrdata_elm.addEventListener("input", () => {
+  generateQrCode(qrdata_elm.value)
+})
